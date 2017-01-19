f49e277b4e6000128519cfb4d8e3788e3156f8dc
lpslpsl <1245441817@qq.com>
2017-01-19 10:11:19 +0800

    大图页

--- app/src/main/java/com/grandmagic/edustore/activity/BigImageViewAct.java
+++ app/src/main/java/com/grandmagic/edustore/activity/BigImageViewAct.java
@@ -7,6 +7,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.grandmagic.edustore.EcmobileApp;
@@ -23,6 +24,7 @@ import uk.co.senab.photoview.PhotoView;
 public class BigImageViewAct extends Activity {
     public static final String IMAGE_ARRAY = "imagepatharray";
     ViewPager viewPager;
+    LinearLayout linearLayout;
     private int position;
     private List<TEACHERCOMMENTS.Img> imagelist;
 
@@ -43,20 +45,38 @@ public class BigImageViewAct extends Activity {
 
     private void initview() {
         viewPager = (ViewPager) findViewById(R.id.viewPager);
+        linearLayout = (LinearLayout) findViewById(R.id.dotlayout);
         photoViews = new ArrayList<>();
         for (TEACHERCOMMENTS.Img s : imagelist) {
             PhotoView photoView = new PhotoView(BigImageViewAct.this);
             photoView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
             ImageLoader.getInstance().displayImage(s.img, photoView, EcmobileApp.options);
             photoViews.add(photoView);
+            ImageView pointView = new ImageView(this);
+            pointView.setPadding(5, 0, 5, 0);
+            pointView.setImageResource(R.drawable.ic_page_indicator);
+            linearLayout.addView(pointView);
         }
         viewPager.setAdapter(new ImgPagerAdapter());
         viewPager.setCurrentItem(position);
+        ((ImageView) linearLayout.getChildAt(position)).setImageResource(R.drawable.ic_page_indicator_focused);
         findViewById(R.id.top_view_back).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
+        });
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {}
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < linearLayout.getChildCount(); i++) {
+                    ((ImageView) linearLayout.getChildAt(i)).setImageResource(i == position ? R.drawable.ic_page_indicator_focused : R.drawable.ic_page_indicator);
+                }
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {}
         });
     }
 
